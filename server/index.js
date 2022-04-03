60a984a4d9b376999b6ef72c15f9074958c8e204
vinh2112 <vuongquocvinh.bh@gmail.com>
2022-04-03 22:03:01 +0700

    add socket backend

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ import payments from "./routers/payments.js";
 import aws from "./routers/aws.js";
 import reports from "./routers/reports.js";
 import dashboard from "./routers/dashboard.js";
+import { Server } from "socket.io";
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
 app.use(cors());
+
 app.use(
   fileupload({
     useTempFiles: true,
@@ -40,12 +42,79 @@ app.use("/api", payments);
 app.use("/api", reports);
 app.use("/api", dashboard);
 
+let onlineUsers = [];
+
+const addNewUser = (userId, socketId) => {
+  !onlineUsers.some((user) => user.userId === userId) && onlineUsers.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return onlineUsers.find((user) => user.userId === userId);
+};
+
 mongoose
   .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to DB");
-    app.listen(PORT, () => {
+    var server = app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
+    });
+
+    const io = new Server(server, {
+      cors: {
+        origin: "https://social-media-lv.netlify.app",
+      },
+    });
+
+    io.on("connection", (socket) => {
+      socket.on("newUser", ({ userId }) => {
+        addNewUser(userId, socket.id);
+        io.emit("getOnlineUsers", {
+          onlineUsers,
+        });
+      });
+
+      socket.on("logoutUser", () => {
+        removeUser(socket.id);
+        io.emit("getOnlineUsers", {
+          onlineUsers,
+        });
+      });
+
+      socket.on("sendUpdateCommentPost", ({ postId }) => {
+        io.emit("getUpdateCommentPost", {
+          postId,
+        });
+      });
+
+      socket.on("sendDeleteComment", ({ comment }) => {
+        io.emit("getDeleteComment", {
+          comment,
+        });
+      });
+
+      socket.on("sendDeleteReply", ({ comment }) => {
+        io.emit("getDeleteReply", {
+          comment,
+        });
+      });
+
+      socket.on("sendOnlineUsers", () => {
+        io.emit("getOnlineUsers", {
+          onlineUsers,
+        });
+      });
+
+      socket.on("disconnect", () => {
+        removeUser(socket.id);
+        io.emit("getOnlineUsers", {
+          onlineUsers,
+        });
+      });
     });
   })
   .catch((err) => {
